e50330de7195fbc36fcc3092536b34701aa5de8d
pshdev1030 <pshdev1030@gmail.com>
2023-04-30 21:09:07 +0900

    rename: 폴더 분리

---
+++ README.md
@@ -0,0 +1,29 @@
+## Types of Tests
+
+### Unit tests
+
+- 작은 단위의 코드를 고립된 환경에서 테스트
+
+### Integration tests
+
+- 두 개 이상의 단위의 코드를 같이 테스팅
+
+### Functional Tests
+
+- 함수의 특정 부분이 소프트웨어에서 잘 동작하는지 테스팅
+
+### Ent-to-end (E2E) Tests
+
+- 실제 브라우저와 서버 환경에서 테스트
+
+## RTL
+
+가상돔으로 테스팅, 브라우저 없이도 테스트 가능
+
+## jest-dom
+
+https://github.com/testing-library/jest-dom
+
+## test(string,callback)
+
+callback에서 에러를 반환하지 않으면 테스트 성공
---
+++ color-button/src/App.test.js
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("button has correct initial color", () => {
+  render(<App />);
+  // find an elemenet with a role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+
+  //expect the background color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+});
+
+test("button has correct initial text", () => {
+  render(<App />);
+});
+test("button turns blue when clicked", () => {
+  render(<App />);
+});
